923f7f58d1dbdd165ece15ae540136387754283f
autorejecttop <autorejecttop@outlook.com>
2026-01-02 16:32:57 +0700

    feat: Finish reading https://docs.flutter.dev/ui/layout

--- layouts/lib/main.dart
+++ layouts/lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 void main() {
+  debugPaintSizeEnabled = true;
+
   runApp(const MainApp());
 }
 
@@ -9,10 +12,29 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    // return Container(
+    //   decoration: BoxDecoration(color: Colors.white),
+    //   child: Center(
+    //     child: Text(
+    //       textDirection: TextDirection.ltr,
+    //       'Hello, world!',
+    //       style: TextStyle(fontSize: 32, color: Colors.black87),
+    //     ),
+    //   ),
+    // );
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        appBar: AppBar(title: Text('Layout')),
+        body: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            // DefaultTextStyle.merge(style: style, child: child)
+            Icon(Icons.star, color: Colors.green[500]),
+            Icon(Icons.star, color: Colors.green[500]),
+            Icon(Icons.star, color: Colors.green[500]),
+            const Icon(Icons.star, color: Colors.black),
+            const Icon(Icons.star, color: Colors.black),
+          ],
         ),
       ),
     );
